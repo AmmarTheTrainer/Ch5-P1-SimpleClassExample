bfd5acc91490ebe05475bc6755d3675c161f5225
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-23 09:45:57 +0500

    Morning commit from hostel

--- Ch5-P1-SimpleClassExample/Car.cs
+++ Ch5-P1-SimpleClassExample/Car.cs
@@ -11,5 +11,41 @@ namespace Ch5_P1_SimpleClassExample
         // The 'state' of the Car.
         public string petName;
         public int currSpeed;
+
+
+        //// A custom default constructor.
+        //public Car()
+        //{
+        //    //Console.WriteLine("default constructor overwrite , Invoked !");
+        //     petName = "Suzuki FX";
+        //     currSpeed = 10;
+        //}
+
+        public Car() => Console.WriteLine("default ctor"); 
+
+        #region Custom Constructors
+
+        //// Here, currSpeed will receive the
+        //// default value of an int (zero).
+        //public Car(string pn)
+        //{
+        //    petName = pn;
+        //}
+        public Car(string pn) => petName = pn;
+
+        // Let caller set the full state of the Car.
+        public Car(string pn, int cs)
+        {
+            petName = pn;
+            currSpeed = cs;
+        }
+        
+        #endregion
+
+        // The functionality of the Car.
+        // Using the expression-bodied member syntax introduced in C# 6
+        public void PrintState() => Console.WriteLine("{0} is going {1} KM/H.", petName, currSpeed);
+        public void SpeedUp(int delta) => currSpeed += delta;
+
     }
 }
--- Ch5-P1-SimpleClassExample/Program.cs
+++ Ch5-P1-SimpleClassExample/Program.cs
@@ -10,9 +10,37 @@ namespace Ch5_P1_SimpleClassExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Lecture 4 - Project 1 - OOP with C# ");
+            Console.WriteLine(" Lecture 4 - Project 1 - OOP with C# \n");
 
+            //// Allocate and configure a Car object.
+            //Car myCar = new Car();
 
+            ////myCar.petName = "Khadim";
+            ////myCar.currSpeed = 10;
+
+            //myCar.PrintState();
+
+            //// Make a Car called Mary going 0 MPH.
+            //Car mary = new Car("Honda City");
+            //mary.PrintState();
+            //// Make a Car called Daisy going 75 MPH.
+            //Car daisy = new Car("Civic", 170);
+            //daisy.PrintState();
+
+            ////// Speed up the car a few times and print out the new state.
+
+            ////for (int i = 0; i <= 10; i++)
+            ////{
+            ////    myCar.SpeedUp(5);
+            ////    myCar.PrintState();
+            ////}
+
+            #region The Default Constructor Revisited
+
+            Motorcycle mc = new Motorcycle(3);
+            mc.PopAWheely();
+
+            #endregion
 
             Console.ReadLine();
         }
